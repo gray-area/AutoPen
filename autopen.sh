c28e182edd7f043164e59a920d9de9d0b85aac4a
gray-area <91284249+gray-area@users.noreply.github.com>
2023-03-03 15:22:40 -0500

    Moved installs to install.sh

--- autopen.sh
+++ autopen.sh
@@ -27,20 +27,6 @@ function purple(){
     fi
 }
 
-# Install Go tools required for functions.
-blue "[+] Installing gowitness..."
-go install github.com/sensepost/gowitness@latest
-blue "[+] Installing httprobe..."
-go install github.com/tomnomnom/httprobe@latest
-blue "[+] Installing assetfinder..."
-go install github.com/tomnomnom/assetfinder@latest
-blue "[+] Installing amass..."
-go install github.com/OWASP/Amass/v3/...@latest
-blue "[+] Installing subjack..."
-go install github.com/haccer/subjack@latest
-
-sudo cp -r /home/$USER/go/bin/* /usr/sbin
-
 
 red "Enter Domain in format: domain.com. Do not place www before."
 
