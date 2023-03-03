886a2e58c9386d1d9cc52870b6e9e3c9aedce503
gray-area <91284249+gray-area@users.noreply.github.com>
2023-03-03 15:21:40 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+#Install extras
+sudo apt install figlet
+sudo apt install libtext-asciitable-perl
+
+# Install Go tools required for functions.
+blue "[+] Installing gowitness..."
+go install github.com/sensepost/gowitness@latest
+blue "[+] Installing httprobe..."
+go install github.com/tomnomnom/httprobe@latest
+blue "[+] Installing assetfinder..."
+go install github.com/tomnomnom/assetfinder@latest
+blue "[+] Installing amass..."
+go install github.com/OWASP/Amass/v3/...@latest
+blue "[+] Installing subjack..."
+go install github.com/haccer/subjack@latest
+
+# Move Go path to user path
+sudo cp -r /home/$USER/go/bin/* /usr/sbin
