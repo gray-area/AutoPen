b863e2653dd6239bd3bb383c30f453c34f4e338f
gray-area <91284249+gray-area@users.noreply.github.com>
2023-03-03 15:23:09 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ sudo apt install figlet
 sudo apt install libtext-asciitable-perl
 
 # Install Go tools required for functions.
+# To check your $GOPATH, enter "go env" into terminal.
+
 blue "[+] Installing gowitness..."
 go install github.com/sensepost/gowitness@latest
 blue "[+] Installing httprobe..."
